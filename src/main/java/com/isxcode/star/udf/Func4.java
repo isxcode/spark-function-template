76f3aa5f6bd7273b0d85a4688c9b9a45942a3b58
ispong <ispong@outlook.com>
2024-10-24 18:22:38 +0800

    优化代码

--- src/main/java/com/isxcode/star/udf/Func4.java
+++ src/main/java/com/isxcode/star/udf/Func4.java
@@ -1,10 +1,7 @@
 package com.isxcode.star.udf;
 
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.DesensitizedUtil;
 import org.apache.spark.sql.api.java.UDF1;
-
-import java.util.Date;
 
 public class Func4 implements UDF1<String, String> {
 
